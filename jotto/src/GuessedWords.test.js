eabf66ad52f925d714ec1208a8b88ed4dd24d4e9
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-12-12 13:14:45 -0600

    add nonzero guess word functionality

--- jotto/src/GuessedWords.test.js
+++ jotto/src/GuessedWords.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {findByTestAttr, checkProps} from '../test/testUtils';
 import GuessedWords from './GuessedWords';
-import { shallow, mount, render } from 'enzyme';
+import {shallow, mount, render} from 'enzyme';
 
 
 const defaultProps = {
@@ -27,21 +27,45 @@ describe('setups', () => {
 
 describe('if there are no words guessed', () => {
     let wrapper;
-    beforeEach(()=> {
+    beforeEach(() => {
         wrapper = setup({guessedWords: []});
     });
-    
-    test('renders without error', () => {        
-        const component = findByTestAttr(wrapper, 'component-guessed-words');        
+
+    test('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
         expect(component.length).toBe(1);
     });
 
     test('renders instructions to guess a word', () => {
-       const instructions = findByTestAttr(wrapper, "guess-instructions");
-       expect(instructions.text().length).not.toBe(0);
+        const instructions = findByTestAttr(wrapper, "guess-instructions");
+        expect(instructions.text().length).not.toBe(0);
     });
 });
 
 describe('if there are words guessed', () => {
+    let wrapper;
+    let guessedWords = [
+        {guessedWord: 'train', letterMatchCount: 3},
+        {guessedWord: 'agile', letterMatchCount: 3},
+        {guessedWord: 'party', letterMatchCount: 3}
+    ];
+    beforeEach(() => {
+        wrapper = setup({guessedWords: guessedWords});
+    });
 
+    test('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
+        expect(component.length).toBe(1);
+    });
+
+    test('renders guessed words section', () => {   
+        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+        console.log(guessedWordsNode.debug());
+        expect(guessedWordsNode.length).toBe(1);
+    });
+
+    test('correct number of guessed words displayed', () => {
+        const guessedWordNodes = findByTestAttr(wrapper,'guessed-word');
+        expect(guessedWordNodes.length).toBe(guessedWords.length);
+    });
 });
