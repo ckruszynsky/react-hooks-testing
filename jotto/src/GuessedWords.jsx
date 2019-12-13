271bdc819c2afb642577c2f96f4a2e673ffa789d
Chad Kruszynsky <chadkruszynsky@Chads-MacBook-Pro.local>
2019-12-12 20:22:59 -0600

    add bootstrap styling

--- jotto/src/GuessedWords.jsx
+++ jotto/src/GuessedWords.jsx
@@ -1,40 +1,40 @@
+import PropTypes from 'prop-types';
+import React from 'react';
 
-import React from 'react'
-import PropTypes from 'prop-types'
+
+function buildInstructionsContainer() {
+    let contents = (<span data-test="guess-instructions">
+        Try to guess the secret word!
+    </span>);
+    return contents;
+}
+
+function buildGuessedWordsTable(props) {
+    const guessedWordRows = props.guessedWords.map((word, index) => (<tr data-test="guessed-word" key={index}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+    </tr>));
+    let contents = (<div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table className="table table-sm">
+            <thead className="thead-light">
+                <tr>
+                    <th>Guess</th>
+                    <th>Matching Letters</th>
+                </tr>
+            </thead>
+            <tbody>
+                {guessedWordRows}
+            </tbody>
+        </table>
+    </div>);
+    return contents;
+}
 
 const GuessedWords = props => {
-    let contents;
-    if(props.guessedWords.length === 0){
-        contents = (
-            <span data-test="guess-instructions">
-                Try to guess the secret word!
-            </span>
-        );
-    }else{
-        const guessedWordRows = props.guessedWords.map((word,index)=> (
-            <tr data-test="guessed-word" key={index}>
-                <td>{word.guessedWord}</td>
-                <td>{word.letterMatchCount}</td>
-            </tr>
-        ));
-
-        contents = (
-            <div data-test="guessed-words">
-                <h3>Guessed Words</h3>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Guess</th>
-                            <th>Matching Letters</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {guessedWordRows}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+    let contents = props.guessedWords.length === 0 ?
+        buildInstructionsContainer()
+        : buildGuessedWordsTable(props);
     return (
         <div data-test="component-guessed-words">
             {contents}
@@ -43,13 +43,14 @@ const GuessedWords = props => {
 }
 
 GuessedWords.propTypes = {
-   guessedWords: PropTypes.arrayOf(
-       PropTypes.shape({
-         guessedWord: PropTypes.string.isRequired,
-         letterMatchCount: PropTypes.number.isRequired  
-       })
-   ).isRequired
-   ,
+    guessedWords: PropTypes.arrayOf(
+        PropTypes.shape({
+            guessedWord: PropTypes.string.isRequired,
+            letterMatchCount: PropTypes.number.isRequired
+        })
+    ).isRequired
+    ,
 }
 
 export default GuessedWords
+
