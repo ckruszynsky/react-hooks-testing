9fd6cee02adf2c088e77a82b4e7af9999b6e17ca
Chad Kruszynsky <chadkruszynsky@Chads-MacBook-Pro.local>
2019-12-13 13:27:49 -0600

    setup input component for tests

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -3,9 +3,22 @@ import React from 'react';
 
 
 export const Input = ({secretWord}) => {
+    const [currentGuess, setCurrentGuess] = React.useState('');
     return (
         <div data-test="component-input">
-            
+            <form className="form-inline">
+                <input
+                    data-test="input-box"
+                    className="mb-2 mx-sm-3"
+                    type="text"
+                    placeholder="Enter your guess"
+                />
+                <button
+                    data-test="submit-button"
+                    className="btn btn-primary mb-2">
+                        Submit
+                </button>
+            </form>
         </div>
     )
 }
