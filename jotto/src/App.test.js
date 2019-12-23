c440a23b85af8372052ef47e3306aa6890d19797
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-12-23 13:18:22 -0600

    added app mount code to setSecretWord

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,19 +1,39 @@
-import { shallow, ShallowWrapper } from 'enzyme';
+import { shallow, ShallowWrapper,mount } from 'enzyme';
 import React from 'react';
 
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
+
 /**
  * Setup function for app component
- * @returns {ShallowWrapper}
+ * @returns {ReactWrapper}
  */
 const setup = () => {
-    return shallow(<App />);
+
+    mockGetSecretWord.mockClear();
+    hookActions.getSecretWord = mockGetSecretWord;
+    
+    /*
+        Use mount, because useEffect not called on `shallow`
+        https://github.com/airbnb/enzyme/issues/2086
+    */
+    return mount(<App />);
 }
 
 test('renders without crashing', ()=> {
     const wrapper = setup();
     const component = findByTestAttr(wrapper,'component-app');
     expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls',()=> {
+    test('getSecretWord is called on App mount', ()=> {
+        setup();
+        //check to see if secret word was updated
+        expect(mockGetSecretWord).toHaveBeenCalled();
+    });
 })
